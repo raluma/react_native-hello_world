911f5e2697fbdea49857a319eeff98cfe40f7414
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-10-01 18:08:18 +0100

    styling tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,21 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import { useState } from 'react';
 
 export default function TabOneScreen() {
+  const [state, setState] = useState(true);
+
+  const handleClick = () => {
+    setState(!state);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Welcome { state ? "👋" : "🙏"}</Text>
+      <Button onPress={handleClick} title="click me" color="blue"
+      accessibilityLabel="blue button" />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
@@ -22,6 +31,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 20
   },
   separator: {
     marginVertical: 30,
